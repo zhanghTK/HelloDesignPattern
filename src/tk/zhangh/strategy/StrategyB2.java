0cc01308dcdbfb3f03680441104d955cb224e3cb
ZhangHao <zhangh_41@163.com>
2016-03-11 19:15:35 +0800

    add strategy proxy

---
+++ src/tk/zhangh/strategy/StrategyB2.java
@@ -0,0 +1,13 @@
+package tk.zhangh.strategy;
+
+/**
+ * Created by zhanghao on 16/3/10.
+ * 策略B接口的具体实现
+ * 实现方法B2
+ */
+public class StrategyB2 implements StrategyB{
+    @Override
+    public void executeStrategy() {
+        System.out.println("Do B2 ...");
+    }
+}
