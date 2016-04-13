68d2013394d8e2d5b858310c353a445702cb28d1
ZhangHao <510223064@qq.com>
2016-04-13 16:00:44 +0800

    update project structure

--- src/tk/zhangh/pattern/command/Command.java
+++
@@ -1,9 +0,0 @@
-package tk.zhangh.pattern.command;
-
-/**
- * Created by ZhangHao on 16/3/20.
- * 命令接口
- */
-public interface Command {
-    void execute();
-}
--- src/tk/zhangh/pattern/proxy/Subject.java
+++
@@ -1,8 +0,0 @@
-package tk.zhangh.pattern.proxy;
-
-/**
- * Created by ZhangHao on 2016/3/8.
- */
-public interface Subject {
-    public void dealTask(String taskName);
-}
