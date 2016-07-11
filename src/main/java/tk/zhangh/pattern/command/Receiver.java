feb8d71a2dd28a651708e8c2a73c40033ed43735
ZhangHao <510223064@qq.com>
2016-07-11 15:37:13 +0800

    update command pattern

--- src/main/java/tk/zhangh/pattern/command/Receiver.java
+++ src/main/java/tk/zhangh/pattern/command/Receiver.java
@@ -1,15 +1,19 @@
 package tk.zhangh.pattern.command;
 
+import org.apache.log4j.Logger;
+
 /**
  * 真正命令执行者，包含若干个命令
  * Created by ZhangHao on 2016/4/13.
  */
 public class Receiver {
+    private static Logger logger = Logger.getLogger(Receiver.class);
+
     public void open(){
-        System.out.println("open command...");
+        logger.info("open command");
     }
 
     public void close(){
-        System.out.println("close command...");
+        logger.info("close command");
     }
 }
