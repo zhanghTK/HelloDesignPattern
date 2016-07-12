9a61e776cfd0ea7db70fd556eb3f63522788e23f
ZhangHao <510223064@qq.com>
2016-07-12 11:37:17 +0800

    add memento pattern

---
+++ src/main/java/tk/zhangh/pattern/memento/CareTaker.java
@@ -0,0 +1,16 @@
+package tk.zhangh.pattern.memento;
+
+/**
+ * Created by ZhangHao on 2016/7/12.
+ */
+public class CareTaker {
+    private EmpMemento memento;
+
+    public EmpMemento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(EmpMemento memento) {
+        this.memento = memento;
+    }
+}
